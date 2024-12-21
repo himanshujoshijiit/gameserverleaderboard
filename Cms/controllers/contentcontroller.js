19e07a9ba48cefff5334f13bd1a84738fd24d5be
himanshujoshijiit <50789959+himanshujoshijiit@users.noreply.github.com>
2024-12-21 22:58:01 +0530

    contentcontroller is updated

--- Cms/controllers/contentcontroller.js
+++ Cms/controllers/contentcontroller.js
@@ -1,25 +1,57 @@
+const e = require('express');
 const content = require('../model/content');
 const validatecontnet = require('../validation/contentvalidation')
 
 
 //create  content
  exports.CreateContent = async (req,res) =>{
-    const {name ,content} = req.body;
-    
-    if(content==null){
-        res.status(400).json{message :"content is empty"};
+    //const {name ,content} = req.body;
+    try{
+      const content = new content(req.body)
+      await content.save();
+      res.status(201).json({message:'content created successfully',data:content});
+    }catch(error ){
+        res.status(400).json({error:error.message})
     }
-
- }
+ };
 
 // update content
 
 exports.UpdateContnet = async (req,res) =>{
-
+    try{
+        const content = await content.findByIdAndUpdate(req.params.id, req.body,{new :true})
+        if(!content){
+            res.status(404).json({message:"content not found"});
+        }
+        res.status(200).json({message:"content created successfully",data:content});
+    }catch(error){
+        res.status(400).json({error:error.message});
+    }
 }
+
+//get all content
+
+exports.getallcontent = async (req,res) =>{
+    try{
+        const contents = await content.findById(req.params.id)
+        if(!content){
+            res.status(404).json({message:"content not found"});
+        }
+        res.status(200).json({message:"content updated successfully"})
+    }catch(error){
+        res.status(400).json({error:error.message});
+    }
+}
+
 
 //delete content
 
 exports.DeleteContent = async (req,res) =>{
-
+     try{
+       const content = await content.findByIdAndDelete(req.params.id);
+       if(!content) res.status(404).json({message:"content not found"});
+       res.status(200).json({message:"content deleted successfully"});
+     }catch(error){
+       res.status(400).json({error:error.message});
+     }
 }
